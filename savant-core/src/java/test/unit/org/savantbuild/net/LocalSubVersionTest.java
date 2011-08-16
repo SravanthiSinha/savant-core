54a717b149e62f1392244ca8f1f4070d75b8f5b7
James Humphrey <leafknode@gmail.com>
2011-08-16 08:03:04 -0600

    Fixed LocalSubVersion test to use a mock subversion repository and project

--- savant-core/src/java/test/unit/org/savantbuild/net/LocalSubVersionTest.java
+++ savant-core/src/java/test/unit/org/savantbuild/net/LocalSubVersionTest.java
@@ -18,40 +18,89 @@ package org.savantbuild.net;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.savantbuild.io.FileTools;
 import org.savantbuild.net.LocalSubVersion.StatusHandler;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
 
 import static org.testng.Assert.*;
 
 /**
  * <p>
- * This class does unit tests for the SubVersion class.
+ * Modified from previous version to use a mock svn repository and project
+ *
+ * This class creates a
  * </p>
  *
- * @author Brian Pontarelli
+ * @author Brian Pontarelli and James Humphrey
  */
 public class LocalSubVersionTest {
+
+  public static File mockSvnRepositoryRoot = new File("target/mock-svn-repository");
+  public static File mockSvnRepositoryTrunk = new File(mockSvnRepositoryRoot, "trunk");
+  public static File mockSvnRepositoryBranches = new File(mockSvnRepositoryRoot, "branches");
+  public static File mockSvnRepositoryBranch_1_0 = new File(mockSvnRepositoryBranches, "1.0");
+  public static File mockSvnRepositoryTags = new File(mockSvnRepositoryRoot, "tags");
+  public static File mockSvnRepositoryTag_1_0_1 = new File(mockSvnRepositoryTags, "1.0.1");
+  public static File mockSvnProject = new File("target/mock-svn-project");
+
+  @BeforeClass
+  public void initMockRepository() {
+    try {
+      FileUtils.deleteDirectory(mockSvnRepositoryRoot);
+      FileUtils.deleteDirectory(mockSvnProject);
+      LocalSubVersion.createRepository(mockSvnRepositoryRoot);
+      LocalSubVersion.makeDirectory(mockSvnRepositoryTrunk);
+      LocalSubVersion.makeDirectory(mockSvnRepositoryBranches);
+      LocalSubVersion.makeDirectory(mockSvnRepositoryBranch_1_0);
+      LocalSubVersion.makeDirectory(mockSvnRepositoryTags);
+      LocalSubVersion.makeDirectory(mockSvnRepositoryTag_1_0_1);
+      LocalSubVersion.checkoutHead(mockSvnRepositoryTrunk, mockSvnProject);
+    } catch (Exception e) {
+      Assert.fail("You must have subversion installed on your system to execute this unit test");
+    }
+
+  }
+
+  /**
+   * Helper method to convert a file path to a SVNURL.toString()
+   *
+   * @param repositoryPath the repository path
+   * @return string representing the svn url
+   */
+  private String repositoryPathToString(File repositoryPath) {
+    try {
+      return SVNURL.fromFile(repositoryPath).toString();
+    } catch (SVNException e) {
+      Assert.fail();
+    }
+    return null;
+  }
+
   @Test(enabled = true)
-  public void url() {
-    String url = LocalSubVersion.getProjectURL(new File("."));
-    assertEquals(url, "https://savant-build.googlecode.com/svn/2.0/savant-core/trunk");
+  public void url() throws SVNException {
+    String url = LocalSubVersion.getProjectURL(mockSvnProject);
+    assertEquals(url, repositoryPathToString(mockSvnRepositoryTrunk));
   }
 
   @Test(enabled = true)
   public void base() {
-    String url = LocalSubVersion.getProjectBaseURL(new File("."));
-    assertEquals(url, "https://savant-build.googlecode.com/svn/2.0/savant-core");
+    String url = LocalSubVersion.getProjectBaseURL(mockSvnProject);
+    assertEquals(url, repositoryPathToString(mockSvnRepositoryRoot));
   }
 
   @Test(enabled = true)
   public void location() {
-    assertEquals(LocalSubVersion.determineLocation("https://savant-build.googlecode.com/svn/2.0/savant-core/trunk"), "trunk");
-    assertEquals(LocalSubVersion.determineLocation("https://savant-build.googlecode.com/svn/2.0/savant-core/branches/1.0"), "branch");
-    assertEquals(LocalSubVersion.determineLocation("https://savant-build.googlecode.com/svn/2.0/savant-core/tags/1.0.1"), "tag");
+    assertEquals(LocalSubVersion.determineLocation(repositoryPathToString(mockSvnRepositoryTrunk)), "trunk");
+    assertEquals(LocalSubVersion.determineLocation(repositoryPathToString(mockSvnRepositoryBranch_1_0)), "branch");
+    assertEquals(LocalSubVersion.determineLocation(repositoryPathToString(mockSvnRepositoryTag_1_0_1)), "tag");
 
     try {
-      assertEquals(LocalSubVersion.determineLocation("https://savant-build.googlecode.com/svn/2.0/savant-core"), "tag");
+      assertEquals(LocalSubVersion.determineLocation(repositoryPathToString(mockSvnRepositoryRoot)), "tag");
       fail("Should have failed");
     } catch (Exception e) {
       // Expected
@@ -60,14 +109,14 @@ public class LocalSubVersionTest {
 
   @Test(enabled = true)
   public void branch() {
-    assertEquals(LocalSubVersion.determineBranch("https://savant-build.googlecode.com/svn/2.0/savant-core/branches/1.0"), "1.0");
-    assertNull(LocalSubVersion.determineBranch("https://savant-build.googlecode.com/svn/2.0/savant-core/tags/1.0.1"));
+    assertEquals(LocalSubVersion.determineBranch(repositoryPathToString(mockSvnRepositoryBranch_1_0)), "1.0");
+    assertNull(LocalSubVersion.determineBranch(repositoryPathToString(mockSvnRepositoryTag_1_0_1)));
   }
 
   @Test(enabled = true)
   public void root() {
-    assertEquals(LocalSubVersion.determineRoot("https://savant-build.googlecode.com/svn/2.0/savant-core/branches/1.0"), "https://savant-build.googlecode.com/svn/2.0/savant-core");
-    assertEquals(LocalSubVersion.determineRoot("https://savant-build.googlecode.com/svn/2.0/savant-core/tags/1.0.1"), "https://savant-build.googlecode.com/svn/2.0/savant-core");
+    assertEquals(LocalSubVersion.determineRoot(repositoryPathToString(mockSvnRepositoryBranch_1_0)), repositoryPathToString(mockSvnRepositoryRoot));
+    assertEquals(LocalSubVersion.determineRoot(repositoryPathToString(mockSvnRepositoryTag_1_0_1)), repositoryPathToString(mockSvnRepositoryRoot));
   }
 
   @Test
@@ -75,7 +124,7 @@ public class LocalSubVersionTest {
     FileTools.write(new File("src/java/test/unit/org/savantbuild/net/svn-changed.txt"), "" + System.currentTimeMillis());
 
     final ThreadLocal<File> holder = new ThreadLocal<File>();
-    LocalSubVersion.doStatus(new File("."), new StatusHandler() {
+    LocalSubVersion.doStatus(mockSvnProject, new StatusHandler() {
       @Override
       public void handle(File file) {
         if (file.getName().equals("svn-changed.txt")) {
