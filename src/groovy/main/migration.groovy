aeff378a35238553a79c36107fece14910d03ddd
Brian Pontarelli <brian@pontarelli.com>
2011-08-19 10:12:25 -0600

    Fixed migration script and fixed commits in dependency path task file to new IJ commit style

--- src/groovy/main/migration.groovy
+++ src/groovy/main/migration.groovy
@@ -8,107 +8,70 @@ import org.savantbuild.io.MD5
  * This script requires the savant-core jar
  */
 
-validateArgs();
+validateArgs()
 
 def validateArgs() {
-  if (args.size() == 0) {
-    println("""You must provide 2 arguments when executing this script.  The first argument
-  is the directory you are migrating from, the second argument is the directory you are migrating to""");
-    System.exit(-1);
+  if (args.size() != 1) {
+    println("""Usage: migration.groovy <dir>
+
+  dir - This is the directory that is the root of the Savant 2.0 repository
+""")
+    System.exit(-1)
   }
 }
 
-File fromDir = new File(args[0]);
-File toDir = new File(args[1]);
-
-AntBuilder ant = new AntBuilder();
-
-ant.delete(dir: toDir)
-ant.mkdir(dir: toDir)
-
-// copy everything from the old savant repo to the new
-ant.copy(toDir: toDir, preservelastmodified: true) {
-  fileset(dir: fromDir);
-}
+File dir = new File(args[0])
 
 // find amd files
-toDir.eachDirRecurse { dir ->
-  dir.eachFileRecurse { file ->
-    migrateAmd(file);
+dir.eachDirRecurse { d ->
+  d.eachFileRecurse { file ->
+    migrateAmd(file)
   }
 }
 
 /**
- * Migrate the amd files.  Marks old ones as ".old" and also updates the md5
+ * Migrate the amd and MD5 files.
  *
  * @param amdFile the amd file
  * @return void
  */
 def migrateAmd(File amdFile) {
   if (amdFile.isFile() && amdFile.getName().endsWith(".amd")) {
-
-    File amdMd5File = new File("${amdFile.absolutePath}.md5");
-
     println("Migrating ${amdFile.getAbsolutePath()}")
 
     // save old
-    AntBuilder ant = new AntBuilder();
-    ant.copy(file: amdFile, toFile: new File("${amdFile.absolutePath}.old").absolutePath, preservelastmodified: true);
-    ant.copy(file: amdMd5File, toFile: new File("${amdMd5File.absolutePath}.old").absolutePath, preservelastmodified: true);
+    String xml = amdFile.getText()
 
-    String oldXml = amdFile.getText();
+//    println("\nOld XML:")
+//    println(xml)
 
-    println("\nOld XML:");
-    println(oldXml);
+    xml = fixCompatTypeBug(xml)
+    xml = fixCloseDependenciesBug(xml)
+    xml = changeArtifactGroupElements(xml)
+    xml = flipGroups(xml)
 
-    String bugFixXml = fixCompatTypeBug(oldXml);
+//    println("\nFixed XML")
+//    println(xml)
 
-    println("\nBug Fixed XML");
-    println(bugFixXml);
+//    println("Updating ${amdFile.getAbsolutePath()} with ${xml}")
+    amdFile.write(xml)
 
-    String migratedXml = migrateArtifactGroups(bugFixXml);
-
-    println("\nMigrated XML");
-    println(migratedXml);
-
-    amdFile.write(migratedXml);
-
-    MD5 md5 = FileTools.md5(amdFile);
-    amdMd5File.write(md5.sum);
+    File amdMd5File = new File("${amdFile.absolutePath}.md5")
+    MD5 md5 = FileTools.md5(amdFile)
+    amdMd5File.write(md5.sum)
   }
 }
 
 /**
  * Parses the old xml, finds the artifacts, and modifies the groups to be the new format
  *
- * @param oldXml the xml from the old amd file
+ * @param xml the xml from the old amd file
  * @return the new, properly formatted xml
  */
-String migrateArtifactGroups(String oldXml) {
-  Node root = new XmlParser().parseText(oldXml);
-  def allNodes = root.depthFirst();
-  allNodes.each {
-    Node node = (Node) it;
-    if (node.name() == 'artifact') {
-      String oldGroup = node.attribute("group");
-      def oldGroupTokens = oldGroup.tokenize(".");
-      def newGroupTokens = [];
-      String newGroup = "";
-      oldGroupTokens.reverseEach { newGroupTokens << it }
-      newGroupTokens.eachWithIndex { newGroupToken, i ->
-        newGroup += newGroupToken;
-        if (i < oldGroupTokens.size() - 1) {
-          newGroup += ".";
-        }
-      }
-
-      node.attributes()["group"] = newGroup;
-    }
+def flipGroups(String xml) {
+  return xml.replaceAll(~"group=\"(.+?)\"") {
+    "group=\"" + it[1].tokenize(".").reverse().join(".") + "\""
   }
-
-  def writer = new StringWriter()
-  new XmlNodePrinter(new PrintWriter(writer)).print(root)
-  return writer.toString()
 }
 
 /**
@@ -122,9 +85,33 @@ String migrateArtifactGroups(String oldXml) {
  * @return
  */
 def fixCompatTypeBug(String xml) {
-  return xml.
-          replace("> compatType=\"minor\"", " compatType=\"minor\"").
-          replace("> compatType=\"major\"", " compatType=\"major\"").
-          replace("> compatType=\"patch\"", " compatType=\"patch\"").
-          replace("> compatType=\"identical\"", " compatType=\"identical\"")
+  return xml.replace("> compatType=", " compatType=")
+}
+
+/**
+ * Some old files contained this:
+ *
+ * </depedencies>
+ *
+ * This method fixes it
+ *
+ * @param node the root xml node
+ * @return
+ */
+def fixCloseDependenciesBug(String xml) {
+  return xml.replace("</depedencies>", "</dependencies>")
+}
+
+/**
+ * Some old files contained this:
+ *
+ * <artifactGroup>
+ *
+ * This method changes to the new name
+ *
+ * @param node the root xml node
+ * @return The fixed XML
+ */
+def changeArtifactGroupElements(String xml) {
+  return xml.replace("artifactGroup", "artifact-group")
 }
